c0413c0a5c23abb450f95b4e81f8f31a7c491f1e
César Román <thecesrom@gmail.com>
2023-02-25 21:14:16 -0800

    feat(vision): add authentication and authorization

--- stubs/incendium/db.pyi
+++ stubs/incendium/db.pyi
@@ -2,12 +2,12 @@ from types import TracebackType
 from typing import Any, List, Optional, Tuple, Type, Union
 
 from com.inductiveautomation.ignition.common import BasicDataset
-from incendium.helper.types import DictIntStringAny, String
+from incendium.helper.types import AnyStr, DictIntStringAny
 
 class DisposableConnection:
-    database: String
+    database: AnyStr
     retries: int
-    def __init__(self, database: String, retries: int = ...) -> None: ...
+    def __init__(self, database: AnyStr, retries: int = ...) -> None: ...
     def __enter__(self) -> DisposableConnection: ...
     def __exit__(
         self,
@@ -16,17 +16,17 @@ class DisposableConnection:
         exc_tb: Optional[TracebackType],
     ) -> None: ...
     @property
-    def status(self) -> String: ...
+    def status(self) -> AnyStr: ...
 
 class Param:
     def __init__(
         self,
-        name_or_index: Union[int, String],
+        name_or_index: Union[int, AnyStr],
         type_code: int,
         value: Optional[Any] = ...,
     ) -> None: ...
     @property
-    def name_or_index(self) -> Union[int, String]: ...
+    def name_or_index(self) -> Union[int, AnyStr]: ...
     @property
     def type_code(self) -> int: ...
     @property
@@ -34,52 +34,52 @@ class Param:
 
 class InParam(Param):
     def __init__(
-        self, name_or_index: Union[int, String], type_code: int, value: Any
+        self, name_or_index: Union[int, AnyStr], type_code: int, value: Any
     ) -> None: ...
 
 class OutParam(Param):
-    def __init__(self, name_or_index: Union[int, String], type_code: int) -> None: ...
+    def __init__(self, name_or_index: Union[int, AnyStr], type_code: int) -> None: ...
 
 def check(
-    stored_procedure: String,
-    database: String = ...,
+    stored_procedure: AnyStr,
+    database: AnyStr = ...,
     params: Optional[List[InParam]] = ...,
 ) -> Optional[bool]: ...
 def execute_non_query(
-    stored_procedure: String,
-    database: String = ...,
-    transaction: Optional[String] = ...,
+    stored_procedure: AnyStr,
+    database: AnyStr = ...,
+    transaction: Optional[AnyStr] = ...,
     params: Optional[List[InParam]] = ...,
 ) -> int: ...
 def get_data(
-    stored_procedure: String,
-    database: String = ...,
+    stored_procedure: AnyStr,
+    database: AnyStr = ...,
     params: Optional[List[InParam]] = ...,
 ) -> BasicDataset: ...
 def get_output_params(
-    stored_procedure: String,
+    stored_procedure: AnyStr,
     output: List[OutParam],
-    database: String = ...,
-    transaction: Optional[String] = ...,
+    database: AnyStr = ...,
+    transaction: Optional[AnyStr] = ...,
     params: Optional[List[InParam]] = ...,
 ) -> DictIntStringAny: ...
 def get_return_value(
-    stored_procedure: String,
+    stored_procedure: AnyStr,
     return_type_code: int,
-    database: String = ...,
-    transaction: Optional[String] = ...,
+    database: AnyStr = ...,
+    transaction: Optional[AnyStr] = ...,
     params: Optional[List[InParam]] = ...,
 ) -> Optional[int]: ...
 def o_execute_non_query(
-    stored_procedure: String,
+    stored_procedure: AnyStr,
     out_params: List[OutParam],
-    database: String = ...,
-    transaction: Optional[String] = ...,
+    database: AnyStr = ...,
+    transaction: Optional[AnyStr] = ...,
     in_params: Optional[List[InParam]] = ...,
 ) -> Tuple[int, DictIntStringAny]: ...
 def o_get_data(
-    stored_procedure: String,
+    stored_procedure: AnyStr,
     out_params: List[OutParam],
-    database: String = ...,
+    database: AnyStr = ...,
     in_params: Optional[List[InParam]] = ...,
 ) -> Tuple[BasicDataset, DictIntStringAny]: ...
