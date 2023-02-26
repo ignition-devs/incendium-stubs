807c0c134f54d53720485866f6d1fbf4d91887a5
César Román <thecesrom@gmail.com>
2023-02-25 21:18:22 -0800

    feat(vision): add authentication and authorization (#35)

--- stubs/incendium/exceptions.pyi
+++ stubs/incendium/exceptions.pyi
@@ -1,19 +1,19 @@
 from typing import Optional
 
-from incendium.helper.types import InnerException, String
+from incendium.helper.types import AnyStr, InnerException
 from java.lang import Throwable
 
 class ApplicationError(Exception):
     cause: Optional[Throwable]
     inner_exception: InnerException
-    message: String
+    message: AnyStr
     def __init__(
         self,
-        message: String,
+        message: AnyStr,
         inner_exception: InnerException = ...,
         cause: Optional[Throwable] = ...,
     ) -> None: ...
 
 class TagError(Exception):
-    message: String
-    def __init__(self, message: String) -> None: ...
+    message: AnyStr
+    def __init__(self, message: AnyStr) -> None: ...
--- stubs/incendium/helper/types.pyi
+++ stubs/incendium/helper/types.pyi
@@ -2,8 +2,8 @@ from typing import Any, Dict, Optional, Union
 
 from java.lang import Exception as JavaException
 
+AnyStr = Union[str, unicode]
 DictIntStringAny = Dict[Union[int, str, unicode], Any]
 DictStringAny = Dict[Union[str, unicode], Any]
 InnerException = Optional[Union[Exception, JavaException]]
 Number = Union[float, int, long]
-String = Union[str, unicode]
