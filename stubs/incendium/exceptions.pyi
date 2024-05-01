9b363c8eb76fda75585aa69db706d335cc4b80f0
César Román <cesar@coatl.dev>
2024-05-01 01:00:54 -0700

    refactor(util): move out of util
    BREAKING CHANGE: deprecate incendium.util module

--- stubs/incendium/exceptions.pyi
+++ stubs/incendium/exceptions.pyi
@@ -3,7 +3,11 @@ from typing import Optional
 from incendium.helper.types import AnyStr, InnerException
 from java.lang import Throwable
 
-class ApplicationError(Exception):
+class Error(Exception):
+    message: AnyStr
+    def __init__(self, message: AnyStr) -> None: ...
+
+class JavaError(Exception):
     cause: Optional[Throwable]
     inner_exception: InnerException
     message: AnyStr
@@ -12,8 +16,27 @@ class ApplicationError(Exception):
         message: AnyStr,
         inner_exception: InnerException = ...,
         cause: Optional[Throwable] = ...,
+        remove_substring: Optional[AnyStr] = ...,
     ) -> None: ...
 
-class TagError(Exception):
-    message: AnyStr
-    def __init__(self, message: AnyStr) -> None: ...
+class ApplicationError(JavaError): ...
+
+class GatewayError(JavaError):
+    def __init__(
+        self,
+        message: AnyStr,
+        inner_exception: InnerException = ...,
+        cause: Optional[Throwable] = ...,
+    ) -> None: ...
+
+class MSSQLError(JavaError):
+    def __init__(
+        self,
+        message: AnyStr,
+        inner_exception: InnerException = ...,
+        cause: Optional[Throwable] = ...,
+    ) -> None: ...
+
+class TagError(Error): ...
+
+def get_function_name() -> AnyStr: ...
